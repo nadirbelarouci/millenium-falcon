02ca5a68cb38bdf8dffec88d8cb7f2698eaaeb58
Nadir Belarouci <nadir.belarrouci@gmail.com>
2023-08-15 12:38:57 +0200

    MF-12 Construct the sqldb path correctly

--- millenium-falcon.sh
+++ millenium-falcon.sh
@@ -12,21 +12,32 @@ if ! command -v jq &> /dev/null; then
     exit 1
 fi
 
-mvn -T1.0C clean package -DskipTests -Dverification.skip -Plocal-dev
+jsonfile=$1
+# Convert relative path to absolute path
+if [[ ! "$jsonfile" = /* ]]; then
+    jsonfile="$PWD/$1"
+fi
+
+# Extract routes_db property
+routes_db=$(jq -r '.routes_db' "$jsonfile")
+dir_of_routes_db=$(dirname "$routes_db")
 
 
-jsonFile="$1"
-jsonFileDir=$(dirname "$jsonFile")
-dirOfRoutesDb=$(jq -r '.routes_db' "$jsonFile" | xargs dirname)
+# Check if routes_db path is relative or absolute
+if [[ ! "$routes_db" = /* ]]; then
+    # Convert relative path to absolute path
+    dir_of_jsonfile=$(dirname "$jsonfile")
+    export SQLDB_PATH="$dir_of_jsonfile/$dir_of_routes_db"
+else
+    export SQLDB_PATH="$dir_of_routes_db"
+fi
 
-# Export the directory path as SQLDB_PATH
-export SQLDB_PATH="$PWD/$jsonFileDir/$dirOfRoutesDb"
-
-jsonWithoutRoutesDb=$(jq -c 'del(.routes_db)' "$jsonFile")
-
-export FALCON_CONFIG="$jsonWithoutRoutesDb"
+# Change JSON content
+routes_db_file_name=$(basename "$routes_db")
+export FALCON_CONFIG=$(jq -c --arg key "routes_db" --arg value "db/$routes_db_file_name" '.[$key]=$value' "$jsonfile")
 
 echo "Falcon config: $FALCON_CONFIG"
 echo "Mounting $SQLDB_PATH to /home/starwars/db"
 
+mvn -T1.0C clean package -DskipTests -Dverification.skip -Plocal-dev
 docker-compose up -d --build
--- r2d2.sh
+++ r2d2.sh
@@ -11,13 +11,29 @@ if ! command -v jq &> /dev/null; then
     exit 1
 fi
 
-FALCON="$1"
-EMPIRE="$2"
 
-falconDir=$(dirname "$FALCON")
-SQLDB_PATH="$PWD/$falconDir/$(jq -r '.routes_db' "$FALCON" )"
-MODIFIED_JSON=$(jq --arg key "routes_db" --arg value "$SQLDB_PATH" '.[$key]=$value' "$FALCON")
+falconJsonFile=$1
+empireJsonFile=$2
+# Convert relative path to absolute path
+if [[ ! "$falconJsonFile" = /* ]]; then
+    jsonfile="$PWD/$1"
+fi
+
+# Extract routes_db property
+routes_db=$(jq -r '.routes_db' "$falconJsonFile")
+
+
+# Check if routes_db path is relative or absolute
+if [[ ! "$routes_db" = /* ]]; then
+    # Convert relative path to absolute path
+    dir_of_jsonfile=$(dirname "$falconJsonFile")
+    SQLDB_PATH="$dir_of_jsonfile/$routes_db/"
+else
+    SQLDB_PATH="$routes_db"
+fi
+
+MODIFIED_JSON=$(jq --arg key "routes_db" --arg value "$SQLDB_PATH" '.[$key]=$value' "$falconJsonFile")
 
 echo $MODIFIED_JSON
 
-java -Dspring.profiles.active=cli -Dspring.application.json="$MODIFIED_JSON" -jar cli/millenium-falcon-app-1.0-SNAPSHOT.jar "$EMPIRE"
+java -Dspring.profiles.active=cli -Dspring.application.json="$MODIFIED_JSON" -jar cli/millenium-falcon-app-1.0-SNAPSHOT.jar "$empireJsonFile"
